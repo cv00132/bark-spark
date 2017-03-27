ba425c42c90c75bd271ac19f9616473e55a30bc4
Tyler Laurenceau <TylerSLaurenceau@gmail.com>
2017-03-27 17:06:04 -0400

    almost have signup working

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,15 @@ import angular from 'angular';
 import 'angular-ui-router';
 import 'angular-cookies';
 
-
 import Config from './config';
+import SERVER from './server';
+
+import UserController from './controllers/user.js';
 
 
 
 angular
-.module('app', ['ui-router', 'ngCookies'])
+.module('app', ['ui.router', 'ngCookies'])
 .config(Config)
+.constant('SERVER', SERVER)
+.controller('UserController', UserController);
