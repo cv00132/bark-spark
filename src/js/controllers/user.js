24c08870f190d006b2bf7415c06634b42e54768d
Tyler Laurenceau <TylerSLaurenceau@gmail.com>
2017-03-27 20:34:59 -0400

    finished signUp page

--- src/js/controllers/user.js
+++ src/js/controllers/user.js
@@ -1,16 +1,17 @@
 import SERVER from '../server'
 
 function UserController($scope, $http, SERVER, $cookies, $state, $rootScope) {
-    $scope.signUp = function(data){
-      $http.post(`${SERVER}/signUp`, data)
-      .then(function(resp) {
-        return 'user successfully created'
-      })
-      .catch(function(error) {
-        console.log(error, "yousuck")
-      });
+    $scope.signUp = function(data) {
+        $http.post(`${SERVER}/signUp`, data)
+        $state.reload()
+            .then(function(response) {
+                console.log(response.data, 'User successfully created');
+            })
+            .catch(function(error) {
+                console.log(error, "You Suck");
+            });
     };
-  }
+}
 
 
 UserController.$inject = ['$scope', '$http', 'SERVER', '$cookies', '$state', '$rootScope'];
